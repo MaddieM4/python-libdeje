d35cb0d0eee6c55e416c9283d18c4b7de2e51a25
Philip Horger <campadrenalin@gmail.com>
2012-11-07 22:17:33 -0800

    setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name = 'deje',
+	version = '0.0.1',
+	description = 'Democratically Enforced JSON Environment library',
+	author = 'Philip Horger',
+	author_email = 'philip.horger@gmail.com',
+	url = 'https://github.com/campadrenalin/python-libdeje/',
+    #scripts = [
+    #    'scripts/ejtpd',
+    #],
+	packages = [
+		'deje',
+		'deje.interpreters',
+	],
+)
