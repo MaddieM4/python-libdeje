643a200f8c2e9a6a1ae521772ae1994b0eae1116
Philip Horger <campadrenalin@gmail.com>
2012-12-02 23:33:42 -0800

    #12 Major initial churn

--- deje/checkpoint.py
+++ deje/checkpoint.py
@@ -15,40 +15,10 @@ You should have received a copy of the GNU Lesser General Public License
 along with python-libdeje.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from ejtp.util.hasher import checksum
-import datetime
-
-DEFAULT_DURATION = datetime.timedelta(minutes = 5)
+import quorum
 
 class Checkpoint(object):
-    def __init__(self, content, version, author = None, signatures = {}):
-        self.content = content
-        self.signatures = dict(signatures)
-        self.version = int(version)
-        self.author = author
-
-    def enact(self, document):
-        document.animus.on_checkpoint_achieve(self.content, self.author)
-
-    def test(self, document):
-        return document.animus.checkpoint_test(self.content, self.author)
-
-    def has_quorum(self, document):
-        write_threshold = document.get_thresholds()['write']
-        if len(self.signatures.keys()) >= write_threshold:
-            return True
-        else:
-            return False
-
-    def hash(self):
-        '''
-        >>> import testing
-        >>> testing.checkpoint().hash()
-        '960341d14e3531a50770bd6529a422d2b4997ae9'
-        '''
-        return checksum([self.content, self.version, self.author])
-
-    def make_signature(self, identity, duration = DEFAULT_DURATION):
+    def __init__(self, document, content, version, author = None, signatures = {}):
         '''
         >>> import testing
         >>> cp = testing.checkpoint()
@@ -69,35 +39,25 @@ class Checkpoint(object):
         Traceback (most recent call last):
         ValueError: No signature provided, and could not derive from identity 'anonymous'
         '''
-        if type(identity) in (str, unicode, bytes):
-            raise ValueError("Identity lookups not available at this time.")
-        expires = (datetime.datetime.utcnow() + duration).isoformat(' ')
-        return expires + "\x00" + identity.sign(expires + self.hash())
+        self.document = document
+        self.content  = content
+        self.version  = int(version)
+        self.author   = author
+        self.quorum   = quorum.Quorum(
+                            self.document, 
+                            [self.content, self.version, self.author],
+                            signatures = signatures,
+                        )
+    def enact(self):
+        self.document.animus.on_checkpoint_achieve(self.content, self.author)
 
-    def verify_signature(self, identity, signature):
-        if type(identity) in (str, unicode, bytes):
-            raise ValueError("Identity lookups not available at this time.")
-        try:
-            expires, subsig = signature.split("\x00", 1)
-        except:
-            # Bad signature format
-            return False
-        expire_date = datetime.datetime.strptime(expires, "%Y-%m-%d %H:%M:%S.%f")
-        plaintext = expires + self.hash()
-        return (expire_date > datetime.datetime.utcnow()) and identity.verify_signature(subsig, plaintext)
+    def test(self):
+        return self.document.animus.checkpoint_test(self.content, self.author)
 
-    def sign(self, identity, signature = None, duration = DEFAULT_DURATION):
-        if type(identity) in (str, unicode, bytes):
-            # String identity
-            if not signature:
-                raise ValueError("No signature provided, and could not derive from identity %r" % identity)
-            identity_name = identity
-        else:
-            # Identity object
-            if signature:
-                if not self.verify_signature(identity, signature):
-                    raise ValueError("Invalid signature")
-            else:
-                signature = self.make_signature(identity, duration)
-            identity_name = identity.name
-        self.signatures[identity_name] = signature
+    def hash(self):
+        '''
+        >>> import testing
+        >>> testing.checkpoint().hash()
+        '960341d14e3531a50770bd6529a422d2b4997ae9'
+        '''
+        return self.quorum.hash
--- deje/quorum.py
+++ deje/quorum.py
@@ -15,14 +15,26 @@ You should have received a copy of the GNU Lesser General Public License
 along with python-libdeje.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from ejtp.util.hasher import checksum
+import datetime
+
+DEFAULT_DURATION = datetime.timedelta(minutes = 5)
+
 class Quorum(object):
-    def __init__(self, document, threshold = "write", signatures = {}):
+    def __init__(self, document, content, threshold = "write", signatures = {}):
+        self.content    = content
         self.document   = document
         self.threshtype = threshold
         self.signatures = dict({})
 
     def sig_valid(self, author):
-        pass
+        identity, signature = self.signatures[author]
+        return validate_signature(ident, self.hash, signature)
+
+    def sign(self, identity, signature = None, duration = DEFAULT_DURATION):
+        if not signature:
+            signature = generate_signature(identity, self.hash, duration)
+        self.signatures[identity.name] = (identity, signature)
 
     @property
     def completion(self):
@@ -46,4 +58,26 @@ class Quorum(object):
 
     @property
     def valid_signatures(self):
-        return { x for x in self.signatures if self.sig_valid(x) }
+        return [ x for x in self.signatures if self.sig_valid(x) ]
+
+    @property
+    def hash(self):
+        return checksum(self.content)
+
+def validate_signature(identity, content_hash, signature):
+    if type(identity) in (str, unicode, bytes):
+        raise ValueError("Identity lookups not available at this time.")
+    try:
+        expires, subsig = signature.split("\x00", 1)
+    except:
+        # Bad signature format
+        return False
+    expire_date = datetime.datetime.strptime(expires, "%Y-%m-%d %H:%M:%S.%f")
+    plaintext = expires + content_hash
+    return (expire_date > datetime.datetime.utcnow()) and identity.verify_signature(subsig, plaintext)
+
+def generate_signature(identity, content_hash, duration = DEFAULT_DURATION):
+    if type(identity) in (str, unicode, bytes):
+        raise ValueError("Identity lookups not available at this time.")
+    expires = (datetime.datetime.utcnow() + duration).isoformat(' ')
+    return expires + "\x00" + identity.sign(expires + content_hash)
