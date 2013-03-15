a4d723a1ded3f4c4bcb8db3346ab76e67e3cf15f
Iuri de Silvio <iurisilvio@gmail.com>
2013-03-16 09:51:37 +1200

    Define valid mimetypes to Resource

--- deje/resource.py
+++ deje/resource.py
@@ -15,9 +15,15 @@ You should have received a copy of the GNU Lesser General Public License
 along with python-libdeje.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import mimetypes
+
 from ejtp.util.py2and3 import *
 
 class Resource(object):
+
+    valid_mimetypes = mimetypes.types_map.values()
+    valid_mimetypes.extend(['text/lua', 'direct/json'])
+
     def __init__(self, path="/", content="", comment="", type="application/x-octet-stream", source=None):
         if source:
             self.deserialize(source)
@@ -49,7 +55,8 @@ class Resource(object):
 
     @type.setter
     def type(self, newtype):
-        # TODO: Verify MIME validity
+        if newtype not in self.valid_mimetypes:
+            raise ValueError('Invalid MIME type: %s' % newtype)
         self._type = String(newtype).export()
         self.trigger_change('type')
 
