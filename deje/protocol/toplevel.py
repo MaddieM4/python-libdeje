4ab492459ccdb22e6e938d15b655c9ecaed1a848
Philip Horger <campadrenalin@gmail.com>
2013-10-26 23:08:13 -0700

    #42 Not-yet-integrated subscription system

--- deje/protocol/toplevel.py
+++ deje/protocol/toplevel.py
@@ -18,15 +18,17 @@ along with python-libdeje.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
 from deje import errors
 
-from deje.protocol.handler  import ProtocolHandler
-from deje.protocol.retrieve import RetrieveHandler
-from deje.protocol.locking  import LockingHandler
+from deje.protocol.handler      import ProtocolHandler
+from deje.protocol.retrieve     import RetrieveHandler
+from deje.protocol.locking      import LockingHandler
+from deje.protocol.subscription import SubscriptionHandler
 
 class DejeHandler(ProtocolHandler):
     def __init__(self, parent):
         ProtocolHandler.__init__(self, parent)
         self._on_retrieve = RetrieveHandler(self)
         self._on_lock     = LockingHandler(self)
+        self._on_sub      = SubscriptionHandler(self)
 
     def _on_error(self, msg, content, ctype, doc):
         sender = msg.sender
